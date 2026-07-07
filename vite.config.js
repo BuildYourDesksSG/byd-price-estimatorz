a050bbb249aae577e02e58c2f7e60e5eae67f9b2
BuildYourDesksSG <info@byd.sg>
2026-07-07 03:06:59 +0100

    Set base path for Vite configuration

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,9 @@
+
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
 export default defineConfig({
+  base: '/byd-price-estimatorz/',
   plugins: [react()],
   test: {
     environment: 'jsdom',
